a8c25eedc123c746354f2ef406c7d365714cdefe
Alan <alan@boxful.com.tw>
2025-10-15 12:16:33 +0800

    adjust

--- flutter/lib/desktop/pages/desktop_setting_page.dart
+++ flutter/lib/desktop/pages/desktop_setting_page.dart
@@ -2231,7 +2231,7 @@ class _AboutState extends State<_About> {
                   ).marginSymmetric(vertical: 4.0)),
               InkWell(
                   onTap: () {
-                    launchUrlString('https://rustdesk.com');
+                    launchUrlString('https://oms.boxful.com.tw');
                   },
                   child: Text(
                     translate('Website'),
@@ -2249,7 +2249,7 @@ class _AboutState extends State<_About> {
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
                           Text(
-                            'Copyright © ${DateTime.now().toString().substring(0, 4)} Purslane Ltd.\n$license',
+                            'Copyright © ${DateTime.now().toString().substring(0, 4)} Boxful Ltd.\n$license',
                             style: const TextStyle(color: Colors.white),
                           ),
                           Text(
